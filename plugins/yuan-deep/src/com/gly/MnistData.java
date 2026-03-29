28c9a92e9bf9ea3966ae9636aac4aafcd782013a
Guoliang-Yang@hotmail.com <YuanIdea@hotmail.com>
2026-03-29 21:30:50 +0800

    Implemented a unified MNIST data processing function.

--- plugins/yuan-deep/src/com/gly/MnistData.java
+++ plugins/yuan-deep/src/com/gly/MnistData.java
@@ -3,33 +3,35 @@ package com.gly;
 import ai.djl.basicdataset.cv.classification.Mnist;
 import ai.djl.ndarray.NDManager;
 import ai.djl.training.dataset.Dataset;
+import ai.djl.training.dataset.RandomAccessDataset;
 import ai.djl.training.util.ProgressBar;
 
+import java.io.IOException;
+
 public class MnistData {
-    public Mnist trainDataset;
-    public Mnist testDataset;
+    public RandomAccessDataset trainDataset;
+    public RandomAccessDataset testDataset;
 
     public void loadData(int batchSize, String engine) {
         try {
-            // 1. 加载数据集
-            trainDataset = Mnist.builder()
-                    .optUsage(Dataset.Usage.TRAIN)
-                    .optManager(NDManager.newBaseManager(engine))
-                    .setSampling(batchSize, true)
-                    .optLimit(Long.MAX_VALUE)
-                    .build();
-            trainDataset.prepare(new ProgressBar());
-
-            testDataset = Mnist.builder()
-                    .optUsage(Dataset.Usage.TEST)
-                    .optManager(NDManager.newBaseManager(engine))
-                    .setSampling(batchSize, false)
-                    .optLimit(Long.MAX_VALUE)
-                    .build();
-            testDataset.prepare(new ProgressBar());
+            // Load the dataset
+            trainDataset = getDataset(Dataset.Usage.TRAIN, batchSize, engine);
+            testDataset = getDataset(Dataset.Usage.TEST, batchSize, engine);
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
+    }
 
+    private static RandomAccessDataset getDataset(Dataset.Usage usage, int batchSize, String engine)
+            throws IOException {
+        Mnist mnist =
+                Mnist.builder()
+                        .optUsage(usage)
+                        .optManager(NDManager.newBaseManager(engine))
+                        .setSampling(batchSize, true)
+                        .optLimit(Long.MAX_VALUE)
+                        .build();
+        mnist.prepare(new ProgressBar());
+        return mnist;
     }
 }
